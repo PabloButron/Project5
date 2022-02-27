8851472b0f8d9b0f478eee46bafb968e278e81eb
Pablo Butron <lizardlabs@icloud.com>
2022-02-26 20:13:45 -0400

    added txt list of words

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -7,8 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
