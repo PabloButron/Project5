fa556cbf3bd178656e5a4bc50888428159f348ce
Pablo Butron <lizardlabs@icloud.com>
2022-03-07 19:41:00 -0400

    Created method to start the game

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -9,25 +9,38 @@ import UIKit
 
 class ViewController: UITableViewController {
     
-    var allWords = [String]()
-    var usedWords = [String]()
     
-    
+    var allWords = [String] ()
+    var userdWords = [String] ()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
-            if let startWords = try? String(contentsOf: startWordsURL) {
+            if let startWords = try? String(contentsOf: startWordsURL){
                 allWords = startWords.components(separatedBy: "\n")
             }
         }
-
         if allWords.isEmpty {
             allWords = ["silkworm"]
         }
-        
+        startGame()
     }
-
-
+    
+    func startGame  (){
+        title = allWords.randomElement()
+        userdWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return userdWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = userdWords[indexPath.row]
+        return cell
+    }
+    
 }
 
